8b90a120bc32385dbb444dad60612997820f97b4
momo <momo@gamil.com>
2023-10-09 18:26:28 +0900

    update

---
+++ src/sdk/dsl/index.ts
@@ -0,0 +1,20 @@
+import { compact, trim } from 'lodash'
+
+class DSLParser {
+  constructor() {
+
+  }
+
+  public parse(text: string) {
+    const formatted = this.format(text)
+  }
+
+  private format(text: string) {
+    const step1 = compact(text.split('\n'))
+    step1.map((element) => {
+      const trimed = trim(element)
+    })
+  }
+}
+
+export default DSLParser
