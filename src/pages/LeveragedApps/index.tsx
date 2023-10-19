874e26b5eb13acd8253862b084a883e9f1c62cbb
momo <momo@gamil.com>
2023-10-20 01:08:36 +0900

    update

--- src/pages/LeveragedApps/index.tsx
+++ src/pages/LeveragedApps/index.tsx
@@ -1,21 +1,30 @@
 import './index.scss'
 
+import classNames from 'classnames'
 import { useMemo, useState } from 'react'
 import { NavLink, Outlet, useLocation } from 'react-router-dom'
 
+import Dialog from '@/components/Dialog'
+
 export default function LeveragedApps() {
   const { pathname } = useLocation()
+
+  const [showDappStore, setShowDappStore] = useState(false)
 
   const [dappList, setDappList] = useState([
     {
       name: 'Uniswap V3',
       // icon: 'shovel',
+      id: 'uniswap',
       title: 'Uniswap V3 - Leveraged LP',
       link: '/leveragedapps/uniswapv3',
     },
     {
       name: 'Spark Protocol',
       title: 'Spark Protocol - Leveraged Lending',
+      id: 'spark',
+      image:
+        'https://2799817246-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FjvdfbhgN5UCpMtP1l8r5%2Fuploads%2Fgit-blob-c029bb6c918f8c042400dbcef7102c4e5c1caf38%2Flogomark%20colour.svg?alt=media',
       link: '/leveragedapps/spark',
     },
     // {
@@ -31,19 +40,63 @@ export default function LeveragedApps() {
 
   return (
     <div className="page-leveraged-apps">
+      <Dialog open={showDappStore} onClose={() => setShowDappStore(false)} className="page-leveraged-apps-store">
+        <h4>Dapp Store</h4>
+        <p>
+          Yield Farming <b>(Up to 7x)</b>
+        </p>
+        <div className="side-bar-app-list">
+          <div className="side-bar-app-list-item side-bar-app-list-item-uniswap">
+            <p>Uniswap V3</p>
+          </div>
+          <div className="side-bar-app-list-item side-bar-app-list-item-spark">
+            <p>spark</p>
+          </div>
+        </div>
+        <p>
+          Trading <b>(Up to 7x)</b>
+        </p>
+        <div className="side-bar-app-list">
+          <div className="side-bar-app-list-item side-bar-app-list-item-uniswap">
+            <p>Uniswap V3</p>
+          </div>
+          <div className="side-bar-app-list-item side-bar-app-list-item-spark">
+            <p>spark</p>
+          </div>
+        </div>
+        <p>
+          Earning <b>(Up to 7x)</b>
+        </p>
+        <div className="side-bar-app-list">
+          <div className="side-bar-app-list-item side-bar-app-list-item-uniswap">
+            <p>Uniswap V3</p>
+          </div>
+          <div className="side-bar-app-list-item side-bar-app-list-item-spark">
+            <p>spark</p>
+          </div>
+        </div>
+      </Dialog>
       <div className="side-bar">
         {/* <h2>Leveraged Apps</h2> */}
         <h4>My Apps</h4>
         <div className="side-bar-app-list">
           {dappList.map((dapp) => {
             return (
-              <NavLink to={dapp.link} className="side-bar-app-list-item" key={dapp.name}>
+              <NavLink
+                to={dapp.link}
+                className={classNames('side-bar-app-list-item', `side-bar-app-list-item-${dapp.id}`)}
+                key={dapp.name}
+              >
                 <p>{dapp.name}</p>
               </NavLink>
             )
           })}
-          <div className="side-bar-app-list-item" key="add">
-            <b>+</b>
+          <div
+            className="side-bar-app-list-item side-bar-app-list-item-add"
+            key="add"
+            onClick={() => setShowDappStore(true)}
+          >
+            <p>Add more</p>
           </div>
         </div>
       </div>
